16463f022cf02ee9e5b469bfd2fc3bf09cb3dd59
alhasantoru <47721611+alhasantoru@users.noreply.github.com>
2020-09-26 00:13:43 +0600

    git

--- learning_users/settings.py
+++ learning_users/settings.py
@@ -79,7 +79,7 @@ WSGI_APPLICATION = 'learning_users.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
